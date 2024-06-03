c17c556736856475392ccfdb31c9f8b5d3565e7e
monir178 <monir.mzs17@gmail.com>
2024-06-03 06:24:59 +0600

    projects added

--- src/pages/home/ProjectCard.tsx
+++ src/pages/home/ProjectCard.tsx
@@ -1,23 +1,57 @@
-"use client";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { PinContainer } from "@/components/ui/pin";
 
 export function ProjectCard() {
   return (
-    <div className="h-[40rem] w-full flex items-center justify-center ">
-      <PinContainer title="youtube" href="https://www.youtube.com/">
-        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
-          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-            Project Name
-          </h3>
-          <div className="text-base !m-0 !p-0 font-normal">
-            <span className="text-slate-500 ">
-              Customizable Tailwind CSS and Framer Motion Components.
-            </span>
+    <div className="w-full h-full">
+      <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+        <Image
+          src="https://st2.depositphotos.com/2001755/8564/i/450/depositphotos_85647140-stock-photo-beautiful-landscape-with-birds.jpg"
+          alt="project name"
+          width={1000}
+          height={1000}
+          className="w-full object-contain"
+        />
+
+        <div className="relative p-4 ">
+          <div>
+            <h1 className="text-xl font-semibold text-purple-100 mb-2">
+              Project Name
+            </h1>
+            {/* <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Details
+              </span>
+            </button> */}
           </div>
-          <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+            <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Details
+              </span>
+            </button>
+            <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Live Site
+              </span>
+            </button>
+            <Link
+              href="https://github.com/monir178/event-360-client"
+              target="_blank">
+              <button className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                  Github
+                </span>
+              </button>
+            </Link>
+          </div>
         </div>
-      </PinContainer>
+      </div>
     </div>
   );
 }
--- src/pages/home/Projects.tsx
+++ src/pages/home/Projects.tsx
@@ -6,8 +6,7 @@ const Projects = () => {
       <h1 className="bg-gradient-to-br from-purple-400 to-purple-100 bg-clip-text text-center text-4xl font-medium tracking-widest uppercase text-transparent md:text-5xl lg:text-7xl mb-20 leading-relaxed">
         Recent Projects
       </h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
