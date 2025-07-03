de5da5957c1799025476af5d5aa562dae13120ac
Moniruzzaman Monir <monir.mzs17@gmail.com>
2025-07-04 00:29:34 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            }
-        ]
-    }
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://your-new-portfolio-link.com/:path*", // replace with your new site URL
+        permanent: true, // sends 308 status for SEO-friendly permanent redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
